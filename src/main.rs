cade4d11d41f9c44e52e98bbbbccb504118b1fe3
TrAyZeN <1810leo@gmail.com>
2020-07-07 20:25:16 +0200

    Lex input

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use std::env;
+
+mod token;
+mod error;
+mod lexer;
+
 fn main() {
-    println!("Hello, world!");
+    let args = env::args().collect::<Vec<String>>()[1..].join(" ");
+
+    let tokens = lexer::tokenize(&args);
+    if let Err(e) = tokens {
+        println!("{}\n{}^\n{}", args, vec![" "; e.index].join(""), e);
+        return;
+    }
+
+    println!("{:?}", tokens);
 }
